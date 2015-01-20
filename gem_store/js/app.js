7b9a7fce2dd238eb33203dcd39035f86413ec865
Luiz Cezer <lccezinha@gmail.com>
2015-01-20 14:55:24 -0200

    ReviewController

--- gem_store/js/app.js
+++ gem_store/js/app.js
@@ -28,6 +28,15 @@ app.controller('PanelController', function() {
   };
 });
 
+app.controller('ReviewController', function() {
+  this.review = {};
+
+  this.addReview = function(product){
+    product.reviews.push(this.review);
+    this.review = {}
+  }
+});
+
 var gems = [
   {
     name: 'Azurite',
