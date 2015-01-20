1b132e1d74edb33af529c20f9b3cc4e9ef9784aa
Luiz Cezer <lccezinha@gmail.com>
2015-01-20 11:59:13 -0200

    WIP: aula 2.3 - panels

--- gem_store/js/app.js
+++ gem_store/js/app.js
@@ -7,24 +7,39 @@ app.controller('StoreController', function() {
 
 var gems = [
   {
-    name: 'Ticaracaticass',
-    price: 2.95,
-    description: 'Blabla',
-    canPurchase: true,
-    soldOut: false,
+    name: 'Azurite',
+    description: "Some gems have hidden qualities beyond their luster, beyond their shine... Azurite is one of those gems.",
+    shine: 8,
+    price: 110.50,
+    rarity: 7,
+    color: '#CCC',
+    faces: 14,
+    images: [ ]
+  },
+  {
+    name: 'Bloodstone',
+    description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
+    shine: 9,
+    price: 22.90,
+    rarity: 6,
+    color: '#EEE',
+    faces: 12,
     images: [
       "images/ruby.png",
       "images/ruby-2.jpeg"
     ]
   },
   {
-    name: 'Bombinhas de biriba',
-    price: 1.95,
-    description: 'Blablazzzzzzzzz',
-    canPurchase: true,
-    soldOut: false,
+    name: 'Zircon',
+    description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
+    shine: 70,
+    price: 1100,
+    rarity: 2,
+    color: '#000',
+    faces: 6,
     images: [
+      "images/ruby-2.jpeg",
       "images/ruby.png"
     ]
   }
-]
+];
