02442bef0d8e69182be10de2231e962f4bd3e378
Luiz Cezer <lccezinha@gmail.com>
2015-01-20 11:02:32 -0200

    ng-src para images

--- gem_store/js/app.js
+++ gem_store/js/app.js
@@ -10,14 +10,19 @@ var gems = [
     name: 'Ticaracaticass',
     price: 2.95,
     description: 'Blabla',
-    canPurchase: false,
-    soldOut: true
+    canPurchase: true,
+    soldOut: false,
+    images: [
+      "images/ruby.png",
+      "images/ruby-2.jpeg"
+    ]
   },
   {
     name: 'Bombinhas de biriba',
     price: 1.95,
     description: 'Blablazzzzzzzzz',
     canPurchase: true,
-    soldOut: false
+    soldOut: false,
+    images: [ ]
   }
 ]
