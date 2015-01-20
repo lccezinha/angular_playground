5c2f4991bd1fee6686f7eafc3034f3cbd4f531c9
Luiz Cezer <lccezinha@gmail.com>
2015-01-20 08:57:59 -0200

    Gemstore - aula 1.2 - controller

--- gem_store/js/app.js
+++ gem_store/js/app.js
@@ -1,1 +1,11 @@
 var app = angular.module('store', []);
+
+app.controller('StoreController', function() {
+  this.product = gem;
+});
+
+var gem = {
+  name: 'Ticaracatica',
+  price: 2.95,
+  description: 'Blabla'
+}
